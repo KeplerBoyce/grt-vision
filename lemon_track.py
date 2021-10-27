6d9374f6716fe8bfcc98b40dd24685067cba428c
KeplerBoyce <keplerboy@gmail.com>
2021-10-27 14:39:31 -0700

    now handles overlapping balls with watershed transform

--- lemon_track.py
+++ lemon_track.py
@@ -1,24 +1,31 @@
-# imports
 from imutils.video import VideoStream
+from skimage.feature import peak_local_max
+from skimage.segmentation import watershed
+from scipy import ndimage
 import numpy as np
 import argparse
 import cv2
 import imutils
 import math
 
+# add argument for choosing video file
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
 	help="path to the (optional) video file")
 args = vars(ap.parse_args())
 
-colorLower = (20, 86, 6)
+# boudns of color range in hsv
+# ranges are h:[0, 180], s:[0, 255], v:[0, 255]
+colorLower = (20, 86, 50)
 colorUpper = (40, 255, 255)
 
+# if no video was chosen, use webcam
 if not args.get("video", False):
     vs = VideoStream(src=0).start()
 else:
     vs = cv2.VideoCapture(args["video"])
 
+# loop through frames
 while True:
     # get frame from video stream
     frame = vs.read()
@@ -33,37 +40,55 @@ while True:
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
     
-    # mask for color
-    # erosions and dilations remove small blobs in mask
-    mask = cv2.inRange(hsv, colorLower, colorUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+    # get mask of areas within color range
+    color_mask = cv2.inRange(hsv, colorLower, colorUpper)
     
-    contour_list = cv2.findContours(mask.copy(), cv2.RETR_TREE,
-        cv2.CHAIN_APPROX_SIMPLE)
-    contour_list = imutils.grab_contours(contour_list)
+    # dilate to fill unwanted holes in mask
+    #   ^(like the logo or any writing on the balls)
+    # over-erode and dilate again to remove random tiny blobs
+    color_mask = cv2.dilate(color_mask, None, iterations=5)
+    color_mask = cv2.erode(color_mask, None, iterations=10)
+    color_mask = cv2.dilate(color_mask, None, iterations=5)
     
-    # check that at least 1 contour was found
-    if contour_list is not None:
+    # find local maxima of distance from edges of mask
+    D = ndimage.distance_transform_edt(color_mask.copy())
+    localMax = peak_local_max(D, indices=False, min_distance=20,
+        labels=color_mask)
     
-        # loop through all contours
-        for contour in contour_list:
+    # watershed transform using local maxima
+    # (splits contours that touch/overlap)
+    markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
+    labels = watershed(-D, markers, mask=color_mask)
+    
+    # loop over labels
+    for label in np.unique(labels):
+        # background has label 0, so ignore it
+        if label == 0:
+            continue
         
-            # find enclosing circle and center of largest contour
-            ((x, y), radius) = cv2.minEnclosingCircle(contour)
-            M = cv2.moments(contour)
-            center = (int(M["m10"]/M["m00"]), int(M["m01"]/M["m00"]))
-            
-            peri = cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, 0.03 * peri, True)
-            
-            # check that contour is not tiny
-            if radius > 10 and len(approx) > 7:
-            
-                # draw circle and center on frame
-                cv2.circle(frame, (int(x), int(y)), int(radius),
-                    (255, 0, 255), 1)
-                cv2.circle(frame, center, 5, (255, 255, 0), -1)
+        new_mask = np.zeros(color_mask.shape, dtype="uint8")
+        new_mask[labels == label] = 255
+        
+        # detect contours in the mask and grab the largest one
+        contour_list = cv2.findContours(new_mask.copy(),
+            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contour_list = imutils.grab_contours(contour_list)
+        contour = max(contour_list, key=cv2.contourArea)
+        
+        # approximate polygon (circle will have a lot of vertices
+        # compared to something like a rectangle, so this lets you
+        # check if the contour is likely circular)
+        hull = cv2.convexHull(contour)
+        approx = cv2.approxPolyDP(hull, 3, True)
+                
+        # bounding circle
+        ((x, y), r) = cv2.minEnclosingCircle(hull)
+        
+        # make sure radius is not tiny and check if seems circular
+        if r > 10 and len(approx) > 7:
+            # draw a circle enclosing the object
+            cv2.circle(frame, (int(x), int(y)), int(r), (0, 255, 0), 1)
+            cv2.circle(frame, (int(x), int(y)), 1, (0, 0, 255), 1)
     
     # show frame
     cv2.imshow("Frame", frame)
